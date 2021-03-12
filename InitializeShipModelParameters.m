cce62b7da1987a38b00b579070ad8fe475089f7d
jaqb1996 <jaqb.klosinski@gmail.com>
2021-03-12 16:09:30 +0100

    Deleted Hb Function

--- InitializeShipModelParameters.m
+++ InitializeShipModelParameters.m
@@ -11,7 +11,7 @@ b3 = 9.77;
 % Steering machine parameters
 N = 12; % [deg/s]
 PB = 5; % [deg]
-deltaMax = 35; % [deg]
+deltaMax = 5; % [deg]
 % Required course settings
 requiredPsi = 15; % [deg]
 stepTime = 0; % [s]
--- RunningModelDemo.m
+++ RunningModelDemo.m
@@ -1,9 +1,9 @@
 clear all; close all;
 InitializeShipModelParameters;
-P = 0.0001;
-I = 0;
+P = 0.1;
+I = 0.0001;
 D = 0;
-simResult = sim('ShipCoureControlModel.slx', 'StopTime', '2000');
+simResult = sim('ShipCoureControlModel.slx', 'StopTime', '5000');
 
 figure;
 plot(simResult.tout, simResult.psi);
