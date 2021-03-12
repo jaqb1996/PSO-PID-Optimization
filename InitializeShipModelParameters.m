f9b25f5333ab2bb4d8ab6263ec8f2ce3e340b710
jaqb1996 <jaqb.klosinski@gmail.com>
2021-03-12 16:39:35 +0100

    Deleted unnecessary parameters

--- InitializeShipModelParameters.m
+++ InitializeShipModelParameters.m
@@ -3,11 +3,6 @@ K = 0.061;
 T1 = 83.5; % [s]
 T2 = 966.3; % [s]
 T3 = 543; % [s]
-% Parameters of nonlinear spiral curve
-b0 = -3.43;
-b1 = -7.32;
-b2 = 2.87;
-b3 = 9.77;
 % Steering machine parameters
 N = 12; % [deg/s]
 PB = 5; % [deg]
--- RunningModelDemo.m
+++ RunningModelDemo.m
@@ -1,8 +1,8 @@
 clear all; close all;
 InitializeShipModelParameters;
-P = 0.1;
-I = 0.0001;
-D = 0;
+P = 1;
+I = 0.001;
+D = 0.0001;
 simResult = sim('ShipCoureControlModel.slx', 'StopTime', '5000');
 
 figure;
