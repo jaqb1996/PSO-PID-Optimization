c66ffd486fb23f49e1fb5e6fc2790f705cf99077
jaqb1996 <jaqb.klosinski@gmail.com>
2021-03-14 13:34:20 +0100

    Added possibility of using non-linear Hb function in the model

--- InitializeShipModelParameters.m
+++ InitializeShipModelParameters.m
@@ -10,3 +10,9 @@ deltaMax = 5; % [deg]
 % Required course settings
 requiredPsi = 15; % [deg]
 stepTime = 0; % [s]
+% Non-linear function Hb parameters
+useHb = true;
+b3 = 9.77;
+b2 = 2.87;
+b1 = -7.32;
+b0 = -3.43;
