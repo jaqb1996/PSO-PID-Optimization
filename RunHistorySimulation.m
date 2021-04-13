1e45ab2136829fe3fcf7e606180f8f1447ebd60b
jaqb1996 <jaqb.klosinski@gmail.com>
2021-04-13 14:50:13 +0200

    added history simulation

--- RunHistorySimulation.m
+++ RunHistorySimulation.m
@@ -5,7 +5,6 @@ Ki_min = 0; Ki_max = 0.5;
 Kd_min = 0; Kd_max = 0.10;
 [numParticles, ~, numIter] = size(history);
 f = figure(1);
-title('Position of particles')
 for iter = 1:numIter
     for p = 1:numParticles
         grid on;
@@ -20,11 +19,11 @@ for iter = 1:numIter
         if whichParticleBest(iter) == p
             style = 'ro';
         end
-        plot3(history(p,1,iter), history(p,2,iter), history(p,3,iter), style);
+        scatter3(history(p,1,iter), history(p,2,iter), history(p,3,iter), style);
         hold on;
     end
     pause(2);
-    if iter ~= num_iter
+    if iter ~= numIter
         clf(f);
     end
 end
